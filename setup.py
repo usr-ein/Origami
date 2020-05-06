ff9c033be3f6fc60bff4afe5b0bd03733c9cba9c
Samuel Prevost <samuel.prevost@pm.me>
2020-05-06 14:48:41 +0200

    Update setup.py to install dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
+with open('README.rst', 'r') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE', 'r') as f:
     license = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read()
 
 setup(
     name='mlmodel',
@@ -15,6 +18,7 @@ setup(
     author_email='samuel.prevost@pm.me',
     url='https://github.com/sam1902/MLModel',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=requirements
 )
 
